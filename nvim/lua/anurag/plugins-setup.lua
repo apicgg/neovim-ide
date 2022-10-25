4e8ffca0d199f8bacef1ab2a280e71be0fbe0193
Anurag Pramanik <p.anurag@outlook.com>
2022-10-25 13:36:03 +0530

    add neosolarized theme

--- nvim/lua/anurag/plugins-setup.lua
+++ nvim/lua/anurag/plugins-setup.lua
@@ -35,6 +35,11 @@ return packer.startup(function(use)
 
 	use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
 
+	use({
+		"svrana/neosolarized.nvim",
+		requires = { "tjdevries/colorbuddy.nvim" },
+	})
+
 	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
 
 	use("szw/vim-maximizer") -- maximizes and restores current window

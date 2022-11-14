581fa32f3eb555faa534525e45bec80b97ed6cb1
Anurag Pramanik <p.anurag@outlook.com>
2022-11-14 10:28:58 +0530

    feat(options): update basic neovim options

--- nvim/lua/user/core/options.lua
+++ nvim/lua/user/core/options.lua
@@ -11,8 +11,8 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
--- opt.wrap = false -- disable line wrapping
-opt.wrap = true -- disable line wrapping
+opt.wrap = false -- disable line wrapping
+-- opt.wrap = true -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
@@ -28,7 +28,7 @@ opt.cursorline = true -- highlight the current cursor line
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
-opt.scrolloff = 8 -- minimal number of screen lines to keep above and below the cursor
+opt.scrolloff = 10 -- minimal number of screen lines to keep above and below the cursor
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -41,3 +41,11 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+
+-- other useful settings
+opt.winblend = 0
+opt.wildoptions = "pum"
+opt.pumblend = 5
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
